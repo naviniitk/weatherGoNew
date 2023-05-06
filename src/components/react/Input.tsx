987ff2b1a99e786be093b3c2f514edbd7bb463fe
Naveen Kumar <naveen.umar@jalantechnologies.com>
2023-05-06 20:02:59 +0530

    integrated openweather 2.5 API

--- src/components/react/Input.tsx
+++ src/components/react/Input.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, useMemo, useState } from "react";
 import "../styles/styles.scss";
 
 interface Props {
@@ -7,17 +7,39 @@ interface Props {
 }
 
 const Input: React.FC<Props> = ({ setWeatherData }: Props) => {
-  const [city, setCity] = useState("");
   const [error, setError] = useState("");
+  const [cityName, setCityName] = useState("");
+
+  const fetchCityData = async (text: string) => {
+    try {
+      const res = await axios.get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${text}.json?access_token=${
+          import.meta.env.PUBLIC_MAP_API_KEY
+        }&cachebuster=1625641871908&autocomplete=true&types=place`
+      );
+      return res.data;
+    } catch (err) {
+      setError(err.message);
+      return { error: "Unable to retrieve places" };
+    }
+  };
+
   const handleSubmit = async (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
     try {
       event.preventDefault();
-      const url =
-        "http://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&APPID=a631e1e5b48cd37e87f8123460fe88e6";
+      const city = await fetchCityData(cityName);
+
+      if (!city.features) {
+        setError("Please enter correct city name!!!");
+        return;
+      }
+      const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${
+        city.features[0].geometry.coordinates[1]
+      }&lon=${city.features[0].geometry.coordinates[0]}&exclude={part}&appid=${
+        import.meta.env.PUBLIC_WEATHER_API_KEY
+      }`;
 
       const response = await axios.get(url);
       setError("");
@@ -27,26 +49,30 @@ const Input: React.FC<Props> = ({ setWeatherData }: Props) => {
     }
   };
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCity(event.target.value);
+    event.preventDefault();
+    setCityName(event.target.value);
   };
 
   const handleReset = () => {
-    setCity("");
+    setCityName("");
   };
 
   return (
     <form className="card card--accent">
       <label className="input">
         <input
+          list="places"
           className="input__field"
           type="text"
           placeholder=" "
-          value={city}
+          value={cityName}
           onChange={handleChange}
+          autoComplete="off"
+          autoFocus
         />
         <span className="input__label">Enter City</span>
       </label>
-      {error && <span className="error">error</span>}
+      {error && <span className="error">{error}</span>}
 
       <div className="button-group">
         <button type="submit" onClick={(e) => handleSubmit(e)}>
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly PUBLIC_MAP_API_KEY: string;
+  readonly PUBLIC_WEATHER_API_KEY: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
